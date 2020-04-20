4fdbe22895a09b0d2f8f680fb555d22a89854e86
Nikita <44400213+nikita-malpani@users.noreply.github.com>
2020-04-20 11:29:44 +0530

    Create Anagram.js

---
+++ Anagram.js
@@ -0,0 +1,7 @@
+function checkAnagram( first, second){
+    return first.toLowerCase().split('').sort().join('') === second.toLowerCase().split('').sort().join('')
+}
+
+console.log(checkAnagram('hello', 'hola'))
+console.log(checkAnagram('friend', 'finder'))
+console.log(checkAnagram('LISTEN', 'silent'))
