63e7cd247aef9fb35c7b30a4ce49e3830f9f3198
Nikita <44400213+nikita-malpani@users.noreply.github.com>
2020-04-20 11:35:28 +0530

    Update Anagram.js

--- Anagram.js
+++ Anagram.js
@@ -1,7 +1,8 @@
-function checkAnagram( first, second){
+function checkAnagram( first, second ){
     return first.toLowerCase().split('').sort().join('') === second.toLowerCase().split('').sort().join('')
 }
 
 console.log(checkAnagram('hello', 'hola'))
 console.log(checkAnagram('friend', 'finder'))
 console.log(checkAnagram('LISTEN', 'silent'))
+console.log(checkAnagram('School master' , 'The classroom'))
