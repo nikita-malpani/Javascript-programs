c6b2bcf9735b96bb0fce6966cbb884348021466f
Nikita <44400213+nikita-malpani@users.noreply.github.com>
2020-05-07 11:12:49 +0530

    Create b.19 Permutations.js

---
+++ b.19 Permutations.js
@@ -0,0 +1,5 @@
+const perm = a => a.length ? 
+                  a.reduce((r, v, i) => [...r, ...perm([...a.slice(0, i), ...a.slice(i + 1)]).map(x => [v, ...x])], []) : 
+                  [[]]
+
+console.log( perm(['a','b','c','d']) )
