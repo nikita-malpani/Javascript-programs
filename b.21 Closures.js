ce1c74741062e279d5edd7ce6f05b39bf9c2c3ac
Nikita <44400213+nikita-malpani@users.noreply.github.com>
2020-05-08 18:04:27 +0530

    Update b.21 Closures.js

--- b.21 Closures.js
+++ b.21 Closures.js
@@ -1,35 +1,44 @@
-function SavingAccount(){
+function SavingAccount() {
   var amount = 0;
- 
+
   return {
-    deposite : function(a){
-      amount += a;
-      console.log(`Deposite of ${a}. Current Balance: ${amount}`)
+    deposite: function (amt) {
+      if (isNaN(amt)) {
+        return `Please enter valid number`
+      }
+      amount += amt;
+      console.log(`Deposite of ${amt}. Current Balance: ${amount}`)
     },
-    withhdrawal : function(a){
-      amount -= a;
-      console.log(`Withdraw of ${a}. Current Balance: ${amount}`)
+    withdrawal: function (amt) {
+      if (amt > amount || isNaN(amt)) {
+        return `Please enter valid number or Amount insufficient`
+      }
+      amount -= amt;
+      console.log(`Withdraw of ${amt}. Current Balance: ${amount}`)
     },
-    balance: function(){
+    balance: function () {
       return amount
     }
   }
 }
-let a1=SavingAccount()
+let a1 = SavingAccount()
+console.log(a1.deposite(undefined))
+console.log(a1.withdrawal(100))
+
 console.log(a1.deposite(100))
 console.log(a1.balance())
-console.log(a1.withhdrawal(10))
+console.log(a1.withdrawal(10))
 console.log(a1.balance())
-console.log(a1.withhdrawal(10))
+console.log(a1.withdrawal(10))
 console.log(a1.balance())
 
-let a2=SavingAccount();
+let a2 = SavingAccount();
 console.log(a2.balance())
 console.log(a2.deposite(1000))
 console.log(a2.balance())
-console.log(a2.withhdrawal(200))
+console.log(a2.withdrawal(200))
 console.log(a2.balance())
-console.log(a2.withhdrawal(100))
+console.log(a2.withdrawal(100))
 console.log(a2.balance())
 
 console.log(a1.balance())
